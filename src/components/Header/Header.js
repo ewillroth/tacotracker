de30963cc9646da51d2cc7aa32c11e3e4de39ffc
Eric Willroth <ewillroth@gmail.com>
2019-06-17 11:03:18 -0500

    add logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,26 +11,44 @@ class Header extends Component{
 			count: '',
 			user: ''
 		}
+		this.login = this.login.bind(this)
 	}
 
 	login(){
 		var provider = new firebase.auth.GoogleAuthProvider();
-		firebase.auth().signInWithPopup(provider).then(function (result) {
+		firebase.auth().signInWithPopup(provider).then(result => {
 			// This gives you a Google Access Token. You can use it to access the Google API.
 			var token = result.credential.accessToken;
 			// The signed-in user info.
 			var user = result.user;
-			// ...
+
+			axios.put('/api/user', {user})
+			.then(response=>{
+				console.log(response)
+				this.setState({user: response.data})
+			}).catch(error=>{
+				console.log(error)
+			})
 		}).catch(function (error) {
 			// Handle Errors here.
 			var errorCode = error.code;
 			var errorMessage = error.message;
+			console.log(error.message);
 			// The email of the user's account used.
 			var email = error.email;
 			// The firebase.auth.AuthCredential type that was used.
 			var credential = error.credential;
 			// ...
 		});
+	}
+
+	logout(){
+		axios.get('/api/logout').then(response=>{
+			console.log(response)
+			this.setState({user:'guest'})
+		}).catch(err=>{
+			console.log(err)
+		})
 	}
 
 	componentDidMount(){
@@ -79,6 +97,7 @@ class Header extends Component{
 				<div className="admincontrols">
 					<Link to="/userpage">View Users</Link>
 					<Link to="/addtacos">Eat Tacos</Link>
+					<button onClick={this.logout}>Logout</button>
 				</div>
 				<div className="usercontrols">
 					<h3>{this.state.user.username}</h3>
