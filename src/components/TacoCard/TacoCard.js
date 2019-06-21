869d4360f22cd0a9fa66c9590a8cd14c4d081806
Eric Willroth <ewillroth@gmail.com>
2019-06-21 08:22:04 -0500

    add TacoCard component

---
+++ src/components/TacoCard/TacoCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rating from '../Rating/Rating';
+import Quantity from '../Quantity/Quantity';
+
+const TacoCard = props => {
+	return (
+		<div>
+
+		</div>
+	)
+}
+
+export default TacoCard;
