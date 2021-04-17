8bbf3a60041f62c4b0a102de28168a0d9c62e1c2
Eric <eric.willroth@projekt202.com>
2021-04-17 09:26:53 -0500

    convert addTacos from class to functional

--- src/components/Addtacos/Addtacos.js
+++ src/components/Addtacos/Addtacos.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 import './Addtacos.css';
 import { Redirect } from 'react-router';
 
-class Addtacos extends Component {
-	constructor() {
-		super();
-		this.state = {
-			quantity: 0,
-			source: '',
-			description: '',
-			rating: 3,
-			redirect: false,
-		};
-	}
+const Addtacos = () => {
+	const [state, setState] = useState({
+		quantity: 0,
+		source: '',
+		description: '',
+		rating: 3,
+		redirect: false,
+	});
 
-	onChange = (e) => {
-		this.setState({ [e.target.name]: e.target.value });
+	const onChange = (e) => {
+		setState({ ...state, [e.target.name]: e.target.value });
 	};
 
-	onSubmit = (e) => {
+	const onSubmit = (e) => {
 		e.preventDefault();
-		const { quantity, rating, description, source } = this.state;
+		const { quantity, rating, description, source } = state;
 		axios
 			.post('/api/tacos', { quantity, rating, description, source })
 			.then((response) =>
-				this.setState({
+				setState({
 					quantity: 0,
 					source: '',
 					description: '',
@@ -36,40 +33,38 @@ class Addtacos extends Component {
 			.catch((err) => console.log(err));
 	};
 
-	render() {
-		return (
-			<div className='body'>
-				{this.state.redirect && <Redirect to='/' />}
-				<form className='addTacoForm' onSubmit={this.onSubmit}>
-					<p>Quantity</p>
-					<input name='quantity' onChange={this.onChange} value={this.state.quantity} type='number' required />
-					<p>From</p>
-					<input name='source' onChange={this.onChange} autoComplete='off' value={this.state.source} type='text' required />
-					<p>Description</p>
-					<textarea
-						name='description'
-						onChange={this.onChange}
-						value={this.state.description}
-						rows='6'
-						cols='40'
-						autoComplete='off'
-						type='text'
-						required
-					/>
-					<p>Rating</p>
-					<input onChange={this.onChange} value={this.state.rating} type='range' min='1' max='5' name='rating' list='ratinglist' required />
-					<button>Submit</button>
-				</form>
-				<datalist id='ratinglist'>
-					<option value='1' />
-					<option value='2' />
-					<option value='3' />
-					<option value='4' />
-					<option value='5' />
-				</datalist>
-			</div>
-		);
-	}
-}
+	return (
+		<div className='body'>
+			{state.redirect && <Redirect to='/' />}
+			<form className='addTacoForm' onSubmit={onSubmit}>
+				<p>Quantity</p>
+				<input name='quantity' onChange={onChange} value={state.quantity} type='number' required />
+				<p>From</p>
+				<input name='source' onChange={onChange} autoComplete='off' value={state.source} type='text' required />
+				<p>Description</p>
+				<textarea
+					name='description'
+					onChange={onChange}
+					value={state.description}
+					rows='6'
+					cols='40'
+					autoComplete='off'
+					type='text'
+					required
+				/>
+				<p>Rating</p>
+				<input onChange={onChange} value={state.rating} type='range' min='1' max='5' name='rating' list='ratinglist' required />
+				<button>Submit</button>
+			</form>
+			<datalist id='ratinglist'>
+				<option value='1' />
+				<option value='2' />
+				<option value='3' />
+				<option value='4' />
+				<option value='5' />
+			</datalist>
+		</div>
+	);
+};
 
 export default Addtacos;
